e2d8965d0fd0c7704df55fb3362631e31e7bda17
Aaron Baker <acbak338@learner.dvusd.org>
2022-03-22 09:27:25 -0700

    3/22

--- json/json/Program.cs
+++ json/json/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using static System.Console;
 
 namespace json
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Warm"
+            };
+            var phoenixForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2022-03-22"),
+                TemperatureCelsius = 28,
+                Summary = "Hot"
+            };
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            WriteLine(jsonString);
+            string phoenixString = JsonSerializer.Serialize(phoenixForecast);
+            WriteLine(phoenixString);
+            WeatherForecast w = JsonSerializer.Deserialize<WeatherForecast>(phoenixString);
+            WriteLine(w.Summary);
+            ReadKey();
         }
     }
 }
--- json/json/weatherForecast.cs
+++ json/json/weatherForecast.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using static System.Console;
 
 namespace json
 {
